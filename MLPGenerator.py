1e10b5db00f5dd45f27ce195a23d83745725d404
unlimitediw <unlimitediw@gwmail.gwu.edu>
2018-12-04 22:58:45 -0500

    MPLGenerator Improve

--- MLPGenerator.py
+++ MLPGenerator.py
@@ -12,13 +12,16 @@ np.random.seed(1)
 
 class MLPGenerator:
 
-    def __init__(self, layerShape, mode, X, Y, X_test, Y_test):
+    def __init__(self, layerShape, mode, X, Y, X_test, Y_test,scale):
+        self.scale = scale
         self.layerShape = layerShape
         self.mode = mode
         self.X = X
         self.Y = Y
         self.X_test = X_test
         self.Y_test = Y_test
+        self.error = 0
+        self.total = 0
 
     # data flatten for sklearn
     def flattenParams(self, Thetas):
@@ -44,13 +47,13 @@ class MLPGenerator:
         return np.array(x).reshape((preSize, self.layerShape[0] + 1))
 
     # weight generation part
-    def genRandThetas(self, epsilon=0.12):
+    def genRandThetas(self, epsilon=0.12,multilier = 1):
 
         # attention, theta is at left
         Thetas = []
         for i in range(len(self.layerShape) - 1):
             theta = np.random.rand(*(self.layerShape[i + 1], self.layerShape[i] + 1)) * 2 * epsilon - epsilon
-            Thetas.append(theta*50)
+            Thetas.append(theta*multilier)
         return Thetas
 
     #
@@ -63,8 +66,7 @@ class MLPGenerator:
             z = theta.dot(features).reshape((theta.shape[0], 1))
             # use sigmoid here, later may change to relu or tanh
             a = expit(z)
-            z_memo.append((z, a))
-            # print(i,z,a)
+            z_memo.append([z, a])
             if i == len(Thetas) - 1:
                 return np.array(z_memo)
             a = np.insert(a, 0, 1)
@@ -75,9 +77,13 @@ class MLPGenerator:
         train_size = len(x) // (self.layerShape[0] + 1)
         x = self.reshapeX(x, train_size)
         total_cost = 0.
-
+        mode = "perceptron"
         for i in range(train_size):
-            hyper = self.propagateForward(x[i].T, Thetas)[-1][1]
+            if mode == "perceptron":
+                # be careful
+                hyper = self.propagateForward(x[i].T, Thetas)[-1][0]
+            else:
+                hyper = self.propagateForward(x[i].T, Thetas)[-1][1]
             cost = self.costFunction(hyper, y[i],mode = 'perceptron')
             total_cost += cost
         total_cost = float(total_cost) / train_size
@@ -88,12 +94,15 @@ class MLPGenerator:
         total_reg *= float(myLambda) / (2 * train_size)
         return total_cost + total_reg
 
-    def costFunction(self, hyper, y, mode='classification'):
+    def costFunction(self, hyper, y, mode='perceptron'):
         if mode == 'classification':
             return - (y * np.log(hyper)) - (1 - y) * (np.log(1 - hyper))
         elif mode == 'perceptron':
-            print(y*(hyper - y))
-            return y * (hyper - y)
+            self.total += 1
+            self.error += abs(hyper-y)
+            print(hyper,self.error/self.total,hyper -y)
+            #return (y-hyper)**.5 if y-hyper > 0 else -(hyper - y)**.5
+            return ((y-hyper))**0.5 if y -hyper >0 else -((y-hyper))**0.5
 
     def gradientFunction(self, z, mode='sigmoid'):
         if mode == 'sigmoid':
@@ -119,9 +128,11 @@ class MLPGenerator:
             for j in range(len(self.layerShape) - 1):
                 zSet.append(temp[j][0])
                 aSet.append(temp[j][1])
-            print(zSet,X)
-            deltas = [(zSet[-1] - y[i])]
-            print(deltas)
+            mode = 'regression'
+            if mode == 'classification':
+                deltas = [np.asarray(aSet[-1] - y[i])]
+            elif mode == 'regression':
+                deltas = [np.asarray(zSet[-1] - y[i])]
             for i in range(1, len(aSet) - 1):
                 t = Thetas[len(Thetas) - i].T[1:,:].dot(deltas[i - 1]) * self.gradientFunction(zSet[len(zSet) - i - 1])
                 deltas.append(t)
@@ -137,8 +148,9 @@ class MLPGenerator:
         return self.flattenParams(DSet).flatten()
 
     def trainNN(self, myLambda=0.):
-        Thetas = self.flattenParams(self.genRandThetas())
+        Thetas = self.flattenParams(self.genRandThetas(multilier=1))
         result = scipy.optimize.fmin_cg(self.computeCost, x0=Thetas, fprime=self.backPropagate, args=(self.flattenX(self.X), self.Y, myLambda),
-                                        maxiter=30,
+                                        maxiter=1000,
                                         disp=True, full_output=True)
+        #print(self.reshapeParams(result[0]))
         return self.reshapeParams(result[0])
